52b6a3b2df25d7f829efb67d8ca27e8cf3a4a947
VPavliashvili <v_pavliashvili@yahoo.com>
2023-09-22 05:11:33 +0400

    instantiation of pillars toward X axis implementation started

--- scripts/pillars_pool.gd
+++ scripts/pillars_pool.gd
@@ -1,14 +1,25 @@
 extends Node
 
+enum Placement { TOP = 1, BOTTOM = -1}
+
 @export var resolution_text: Label
 var pillar_prefab = preload("res://scenes/pillar.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	instantiate_pillar_on_top()
+
+	var top_counter: int = 0
+	while top_counter < 10:
+		instantiate_pillar(Placement.TOP, top_counter)
+		top_counter += 1
+
+	instantiate_pillar(Placement.BOTTOM, 0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	pass
+
+func change_text() -> void:
 	var res = get_screen_width_height()
 	var format := "[W:%s;H:%s]"
 	resolution_text.text = format % [str(res.x), str(res.y)]
@@ -16,12 +27,15 @@ func _process(_delta):
 func get_screen_width_height() -> Vector2i:
 	return DisplayServer.window_get_size()
 
-func instantiate_pillar_on_top() -> void:
+func instantiate_pillar(placement: Placement, line_number: int) -> void:
 	var pillar: Node2D = pillar_prefab.instantiate() as Node2D
 	add_child(pillar)
-	set_top_pillar_position(pillar)
+	if placement == Placement.TOP:
+		set_top_pillar_position(pillar, line_number)
+	else:
+		set_bottom_pillar_position(pillar)
 
-func set_top_pillar_position(pillar: Node2D) -> void:
+func set_top_pillar_position(pillar: Node2D, line_number: int) -> void:
 	var sprite: Sprite2D = pillar.get_node("visual")
 	var size: Vector2i = sprite.texture.get_size()
 	# var screen: Vector2i = get_screen_width_height()
@@ -30,6 +44,9 @@ func set_top_pillar_position(pillar: Node2D) -> void:
 
 	# 2.0 since I need half of the size of whole sprite to make it touch the upper end of the screen
 	var pos_y : float = float(screen_y) + float(size.y) / 2.0 * pillar.scale.y
-	var pos_x : int = 500
-	
+	var pos_x : int = 500 + line_number * 100
+
 	pillar.position = Vector2(pos_x, pos_y)
+
+func set_bottom_pillar_position(_pillar: Node2D) -> void:
+	change_text()
