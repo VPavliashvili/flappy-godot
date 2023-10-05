7dce0f276ea717d93538692b51653fe4b6ddaafb
VPavliashvili <v_pavliashvili@yahoo.com>
2023-10-06 02:41:14 +0400

    pillars pooling refactored

--- scripts/pillars_pool.gd
+++ scripts/pillars_pool.gd
@@ -23,17 +23,26 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	manage_pooling()
+
+
+func manage_pooling():
 	if last_column != null && not has_found_respawn_point_x:
-		if last_column.position.x - distance_beetween_pillar_x < screen.x:
-			respawn_point_x = last_column.position.x + distance_beetween_pillar_x
+		if is_close_to_screen_border():
+			var size_x: float = last_column.get("size_x")
+			respawn_point_x = last_column.position.x + distance_beetween_pillar_x + size_x
 			has_found_respawn_point_x = true
 	
 	if has_found_respawn_point_x:
-		if last_column.position.x - distance_beetween_pillar_x < screen.x:
+		if is_close_to_screen_border():
 			var head: Node2D = pool.pop_front() as Node2D
 			reset_column_pos_as_last(head, respawn_point_x)
 			pool.push_back(head)
 			last_column = head
+
+
+func is_close_to_screen_border() -> bool:
+	return last_column.position.x - distance_beetween_pillar_x < screen.x
 
 
 func instantiate_columns() -> void:
@@ -60,27 +69,13 @@ func set_column_pos_upon_spawn(column: Node2D, prev_pos_y: int, start_pos_x: int
 	var pos_x: int = start_pos_x + counter * (size_x + distance_beetween_pillar_x)
 
 	var path_heigh = column.get("path_heigh")
-	var pos_y: float
-
-	var distance_from_border: float
-	var offset_from_border: int = 50
-	
-	if prev_pos_y > screen.y / 2:
-		distance_from_border = screen.y - (prev_pos_y + path_heigh / 2)
-		if distance_from_border <= offset_from_border:
-			pos_y = prev_pos_y - path_heigh / 2
-			column.position = Vector2(pos_x, pos_y)
-			# print("name -> {1}, pos -> {2}".format([column.name, Vector2(pos_x, pos_y)]))
-			return
-	else:
-		distance_from_border = prev_pos_y - path_heigh / 2
-		if distance_from_border <= offset_from_border:
-			pos_y = prev_pos_y + path_heigh / 2
-			column.position = Vector2(pos_x, pos_y)
-			# print("name -> {0}, pos -> {1}".format([column.name, Vector2(pos_x, pos_y)]))
-			return
 		
+	var check_border = check_going_past_edge_and_get_value(path_heigh, prev_pos_y)
+	if check_border.is_on_edge:
+		column.position = Vector2(pos_x, check_border.pos_y)
+		return
 
+	var pos_y: float
 	var should_add: bool = rng.randi_range(1, 100) > 50
 	if should_add:
 		pos_y = prev_pos_y + path_heigh / 2
@@ -93,23 +88,11 @@ func set_column_pos_upon_spawn(column: Node2D, prev_pos_y: int, start_pos_x: int
 func reset_column_pos_as_last(column: Node2D, pos_x: float) -> void:
 	var path_heigh = column.get("path_heigh")
 	var pos_y: float
-
-	var distance_from_border: float
-	var offset_from_border: int = 50
-	
-	if last_column.position.y > screen.y / 2:
-		distance_from_border = screen.y - (last_column.position.y + path_heigh / 2)
-		if distance_from_border <= offset_from_border:
-			pos_y = last_column.position.y - path_heigh / 2
-			column.position = Vector2(pos_x, pos_y)
-			return
-	else:
-		distance_from_border = last_column.position.y - path_heigh / 2
-		if distance_from_border <= offset_from_border:
-			pos_y = last_column.position.y + path_heigh / 2
-			column.position = Vector2(pos_x, pos_y)
-			return
 		
+	var check_border = check_going_past_edge_and_get_value(path_heigh, last_column.position.y)
+	if check_border.is_on_edge:
+		column.position = Vector2(pos_x, check_border.pos_y)
+		return
 
 	var should_add: bool = rng.randi_range(1, 100) > 50
 	if should_add:
@@ -118,4 +101,23 @@ func reset_column_pos_as_last(column: Node2D, pos_x: float) -> void:
 		pos_y = last_column.position.y - path_heigh / 2
 
 	column.position = Vector2(pos_x, pos_y)
+
+
+func check_going_past_edge_and_get_value(path_heigh: float, prev_pos_y: float):
+	var distance_from_border: float
+	var offset_from_border: int = 50
+	var pos_y: float
+
+	if prev_pos_y > screen.y / 2:
+		distance_from_border = screen.y - (prev_pos_y + path_heigh / 2)
+		if distance_from_border <= offset_from_border:
+			pos_y = prev_pos_y - path_heigh / 2
+			return { "is_on_edge": true, "pos_y": pos_y }
+	else:
+		distance_from_border = prev_pos_y - path_heigh / 2
+		if distance_from_border <= offset_from_border:
+			pos_y = prev_pos_y + path_heigh / 2
+			return { "is_on_edge": true, "pos_y": pos_y }
+	
+	return { "is_on_edge": false, "pos_y": 0 }
 
